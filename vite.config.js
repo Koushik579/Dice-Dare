d6a7171eeef88a74d6fe17b17a4f22bae4aa2b9d
Koushik579 <koushikkarmakar535@gmail.com>
2026-09-02 22:25:22 +0530

    bug fix 1

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages serves this repository under /night-game/.
+  base: '/night-game/',
   plugins: [react()],
 })
